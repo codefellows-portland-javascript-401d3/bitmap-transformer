64b50d9c485f1511978c316339e5a13fc6a6950e
Danielle H <daheber@gmail.com>
2016-07-14 14:38:20 -0700

    comments for hints/definitions

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ function getBufferHeader(){
     const numColorsInPalette = buffer.readInt16LE(46);
     console.log(offset);
     console.log(numColorsInPalette);
-    console.log(buffer);
   });
 
 }
 
 getBufferHeader();
+
+//buffer = entire file
+//offset = start of the header
+//num of colors in palette //we just need to find rgb
+//4bits per color RGBA
+//ends at 1094
+//functions take in dword => alter and bring dword back //dword
