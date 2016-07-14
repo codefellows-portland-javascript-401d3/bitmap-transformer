8134414a2827c04551e5b60052f84d92208214bf
Arielle Foldoe <afoldoe@gmail.com>
2016-07-14 12:08:36 -0700

    set up project skeleton

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change Log
+This files contains all the documented changes in this project. The project adheres to Semantic Versioning (http://semver.org/)
+
+
+
+[0.1.0] - 2016-07-14
+#added
+Added project skeleton
+
---
+++ test/bitmap.test.js
@@ -0,0 +1,48 @@
+const chai = require('chai').assert();
+const fs = require('fs');
+
+
+
+
+
+
+describe('file', () => {
+  it('opens using fs', function() {
+    
+  });
+
+  it('can be read into a buffer', function() {
+    
+  });
+
+  it('header data stored in data object', function() {
+    
+  });
+
+  it('run transformer on buffer', function() {
+    
+  });
+
+  it('write the buffer to a new file', function() {
+    
+  });
+});
+
+describe('api calls', () => {
+  it('invert colors', function() {
+
+  });
+
+  it('gray scale colors', function() {
+    
+  });
+
+  it('red shift colors', function() {
+    
+  });
+});
+
+
+//api for inteferfacing with the trandform by the files names
+//cli 
+//cli to select transformer
