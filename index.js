6dd85c5cf6c3e3c49f03ebda6379df7158573eeb
Danielle H <daheber@gmail.com>
2016-07-15 12:58:52 -0700

    invert functionality complete

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const transform = require('./lib/transform');
-const invert = require('./lib/invert');
-const write = require('./lib/write');
+//const invert = require('./lib/invert');
+//const write = require('./lib/write');
 // const grayscale = require('./lib/grayscale');
 
 function invertIt () {
-  transform('./img/palette-bitmap.bmp', invert, write);
+  transform('./img/palette-bitmap.bmp');
 }
 invertIt();
 
