90560da463606a2610ca880b76e9d08281fa67d4
Kate Sowles <katesowles@gmail.com>
2016-07-16 14:34:13 -0700

    blue shift, complete

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
 const transform = require('./lib/transform');
-//const invert = require('./lib/invert');
-//const write = require('./lib/write');
-// const grayscale = require('./lib/grayscale');
 
-function invertIt () {
-  transform('./img/palette-bitmap.bmp');
-}
-invertIt();
-
-// function grayscaleIt () {
-//   transform('./img/palette-bitmap.bmp', grayscale);
-// }
-// grayscaleIt();
+transform('./img/palette-bitmap.bmp');
